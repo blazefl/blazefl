ae847c3b08cffb7a086b903196f3a63864417137
kitsuyaazuma <kitsuyaazuma@gmail.com>
2025-07-09 23:24:43 +0900

    feat: use enum for model names in model selector
    Signed-off-by: kitsuyaazuma <kitsuyaazuma@gmail.com>

--- src/blazefl/core/model_selector.py
+++ src/blazefl/core/model_selector.py
@@ -1,9 +1,12 @@
-from typing import Protocol
+from enum import StrEnum
+from typing import Protocol, TypeVar
 
 import torch
 
+ModelName = TypeVar("ModelName", bound=StrEnum, contravariant=True)
 
-class ModelSelector(Protocol):
+
+class ModelSelector(Protocol[ModelName]):
     """
     Abstract base class for selecting models in federated learning.
 
@@ -14,12 +17,12 @@ class ModelSelector(Protocol):
         NotImplementedError: If the method is not implemented in a subclass.
     """
 
-    def select_model(self, model_name: str) -> torch.nn.Module:
+    def select_model(self, model_name: ModelName) -> torch.nn.Module:
         """
         Select and return a model instance by its name.
 
         Args:
-            model_name (str): The name of the model to select.
+            model_name (ModelName): The name of the model to select.
 
         Returns:
             torch.nn.Module: An instance of the selected model.
