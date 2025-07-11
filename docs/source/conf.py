0af3d96a79a82d8a90539a2f4d5b38142b7f93db
kitsuyaazuma <kitsuyaazuma@gmail.com>
2025-07-11 21:22:44 +0900

    docs: add root src directory
    Signed-off-by: kitsuyaazuma <kitsuyaazuma@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,7 @@ release = importlib.metadata.version(project)
 import sys  # noqa: E402
 from pathlib import Path  # noqa: E402
 
-sys.path.insert(0, str(Path("../../src/blazefl/core").resolve()))
-sys.path.insert(0, str(Path("../../src/blazefl/contrib").resolve()))
-sys.path.insert(0, str(Path("../../src/blazefl/reproducibility").resolve()))
+sys.path.insert(0, str(Path("../../src").resolve()))
 
 extensions = [
     "sphinx.ext.napoleon",
